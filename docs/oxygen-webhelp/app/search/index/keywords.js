28e93de1507ffba78fcaaf084377ffde9d685545
Kacper Bojakowski <kacperbojakowski@gmail.com>
2022-04-12 21:13:16 +0200

    Add files via upload.

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,20 @@
+define(function() {var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Castration",p:["p1"]},{w:"Cat",p:["p2"]},{w:"breeds",p:["p2"]},{w:"Feeding",p:["p3"]},{w:"General",p:["p4"]},{w:"information",p:["p4"]},{w:"Health",p:["p5"]},{w:"problems",p:["p5"]},{w:"How",p:["p6","p7"]},{w:"to",p:["p6","p7"]},{w:"get",p:["p6"]},{w:"a",p:["p6"]},{w:"cat?",p:["p6","p7"]},{w:"understand",p:["p7"]},{w:"your",p:["p7"]},{w:"Hygiene",p:["p8"]},{w:"Maintenance",p:["p9"]},{w:"cost",p:["p9"]},{w:"My",p:["p10"]},{w:"First",p:["p10"]},{w:"Topic",p:["p10"]},{w:"Play",p:["p11"]},{w:"Training",p:["p12"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3];
+ph["p2"]=[4, 5];
+ph["p3"]=[6];
+ph["p4"]=[7, 8];
+ph["p5"]=[9, 10];
+ph["p6"]=[11, 12, 13, 14, 15];
+ph["p7"]=[11, 12, 16, 17, 15];
+ph["p8"]=[18];
+ph["p9"]=[19, 20];
+ph["p10"]=[21, 22, 23];
+ph["p12"]=[25];
+ph["p11"]=[24];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {2:-1,5:-1,9:-1,7:-1,12:-1,8:-1,11:-1,4:-1,6:-1,3:-1,1:-1};
+});
